1a49dfd70bc6e841ac7c8c3c5ef0f3507a96595e
mushkin-v <mushkin.v@gmail.com>
2015-05-21 19:11:57 +0300

    add fixtures for employees and categories

--- src/AppBundle/DataFixtures/ORM/LoadData.php
+++ src/AppBundle/DataFixtures/ORM/LoadData.php
@@ -1,6 +1,8 @@
 <?php
+
 namespace AppBundle\DataFixtures\ORM;
 
+use Application\Sonata\MediaBundle\Entity\Media;
 use Hautelook\AliceBundle\Alice\DataFixtureLoader;
 
 class LoadData extends DataFixtureLoader
@@ -8,8 +10,21 @@ class LoadData extends DataFixtureLoader
     protected function getFixtures()
     {
         return array(
-            __DIR__.'/fixturesEmployee.yml',
-            __DIR__.'/fixturesCategory.yml',
+            __DIR__.'/fixturesCategory_uk.yml',
+            __DIR__.'/fixturesEmployee_uk.yml',
         );
+    }
+
+    public function getMedia($name, $context = 'default')
+    {
+        $media = new Media();
+
+        $media->setBinaryContent(__DIR__.'/../data/'.$name);
+        $media->setContext($context);
+        $media->setProviderName('sonata.media.provider.image');
+
+        $this->container->get('sonata.media.manager.media')->save($media, $andFlush = true);
+
+        return $media;
     }
 }
